b37a9cc2d52179dad1741807b907ebf43d10072c
Pamela Lima <pamelalima@Pamelas-Air.lan>
2021-10-15 23:26:15 -0700

    Added the APIs for login, reg, and posts

--- server/index.js
+++ server/index.js
@@ -2,20 +2,31 @@ const express = require("express")
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/post");
+
+dotenv.config();
 
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/test').then(
+    await mongoose.connect(process.env.MONGO_URL).then(
         console.log("Connected to MongoDB")
     );
 }
 
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
 app.use(cors());
 
-app.get("/api/test", (req, res) => {
-    res.send('{"message": "Working"}');
-})
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/post",postRoute);
 
 app.listen(8800, () =>{
     console.log("Server Running")
---
+++ server/routes/users.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+
+//update user
+// router.put("/:id")
+//delete user
+// get a user
+// follow a user
+// unfollow a user
+
+module.exports = router;
